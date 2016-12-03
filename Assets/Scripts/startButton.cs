266d37eda31658d61f42548ebcf1c63031613ae4
Henry <hlee2052@gmail.com>
2016-12-03 15:07:01 -0800

    start button works

---
+++ Assets/Scripts/startButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class startButton : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(()=>
+        {
+            SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
