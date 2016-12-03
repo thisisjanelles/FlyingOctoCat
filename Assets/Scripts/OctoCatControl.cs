d3852456d82d601055bd9e0b1fc98f67569bb7a9
Eric Kim <bosung90@gmail.com>
2016-12-03 13:51:17 -0800

    Flying box with touch to fly up

---
+++ Assets/Scripts/OctoCatControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctoCatControl : MonoBehaviour {
+
+	private Rigidbody2D _rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		_rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+			_rigidBody.velocity = Vector2.up * 4;
+		}
+	}
+}
