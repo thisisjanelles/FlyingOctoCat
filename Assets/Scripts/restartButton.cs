20892a26fd77b8fd64c5a9d7258abdc1fe4aa2b1
Henry <hlee2052@gmail.com>
2016-12-03 16:00:41 -0800

    replay button returns main

---
+++ Assets/Scripts/restartButton.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class restartButton : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Main", LoadSceneMode.Additive);
+        });
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
+
+
+
