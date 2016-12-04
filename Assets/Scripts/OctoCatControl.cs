ffeb591ab0ac8b5c73f8aa2a823388e5061aab15
Henry <hlee2052@gmail.com>
2016-12-03 19:40:54 -0800

    added SS and added death

--- Assets/Scripts/OctoCatControl.cs
+++ Assets/Scripts/OctoCatControl.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OctoCatControl : MonoBehaviour {
 
 	private Rigidbody2D _rigidBody;
-
+    private Transform _transform;
 	// Use this for initialization
 	void Start () {
 		_rigidBody = GetComponent<Rigidbody2D> ();
-	}
+        _transform = GetComponent<Transform>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,6 +20,9 @@ public class OctoCatControl : MonoBehaviour {
 			_rigidBody.velocity = Vector2.up * 4 + Vector2.right * 0.5f;
 		}
 
-        
+        if(_transform.position.x < -15 || _transform.position.y < -15 || _transform.position.y > 15)
+        {
+            SceneManager.LoadScene("gameOver");
+        }
 	}
 }
