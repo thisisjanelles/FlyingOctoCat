a3e52beb4b4fe839938cbcc008d7a0e98deaa78c
Wan <whirligig@hotmail.com>
2016-12-03 17:00:16 -0800

    Energy bar is working.

--- Assets/EnergyBarAdvance.cs
+++ Assets/EnergyBarAdvance.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnergyBarAdvance : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Image _energyBar;
+    
+    void Start()
+    {
+        _energyBar = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _energyBar.fillAmount = Time.time * 0.05f;
+     }
+
+    float getFillAmount()
+    {
+        return _energyBar.fillAmount;
+    }
+
+    void shrink()
+    {
+        if (_energyBar.fillAmount > 1/3) {
+            _energyBar.fillAmount -= 1 / 3;
+        } else if (_energyBar.fillAmount > 2 / 3) {
+            _energyBar.fillAmount -= 2 / 3;
+        } else if (_energyBar.fillAmount > 0.999f) {
+            _energyBar.fillAmount = 0;
+        }
+    }
 }
