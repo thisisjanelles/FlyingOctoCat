4d4aaee4bdc28cfc84e6d1127d76010e4f58b7eb
Henry <hlee2052@gmail.com>
2016-12-03 16:46:28 -0800

    background image added + moving

---
+++ Assets/Scripts/moveLeft.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveLeft : MonoBehaviour {
+
+    private RectTransform _transform;
+
+	// Use this for initialization
+	void Start () {
+        _transform = GetComponent<RectTransform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        _transform.position = _transform.position - Vector3.right*0.05f;
+        if (_transform.position.x < -20)
+        {
+            _transform.localPosition = _transform.localPosition + new Vector3(_transform.rect.width * 3f, 0, 0);
+        }
+    }
+}
